0bce033ad677b40e2970bc16d6aeefffca5002dc
ben1mit <benemit@online.no>
2020-03-26 13:11:34 +0100

    changed the documentation to contain the whole project

--- main_project/main/main.c
+++ main_project/main/main.c
@@ -1,9 +1,43 @@
 /**
+ * @mainpage
+ * This is a project made for the subject "electronic system design, project II" at NTNU. \n
+ * The project consists of two units: \n
+ * One which detects bird-collisions at the blade of the windmill, 
+ * based on vibration patterns detected with an accelerometer, and sends a signal via Blluetooth Low Energy \n
+ * And a second, called the hub, which relays a signal recieved via BLE to our database via WIFI. \n \n
+ * 
+ * The documentation for the unit on the wing is found in main_project, and the code for the hub is found in hub_project \n
+ * To understand the project, look at the documentation of the header-files and main-files in these projects first \n \n
+ * 
+ * To run this project a few things must be done: \n
+ * download ESP-IDF, this can be done from here: https://docs.espressif.com/projects/esp-idf/en/latest/esp32/get-started/.
+ * (Building and flashing the project is also decribed there) \n \n
+ * 
+ * You must also edit the file at esp-idf/components/partition_table/partitions_singleapp.csv, here "factory, app" size should be  0x180000 not 1M. \n \n
+ * 
+ * Menuconfig must be set up correctly: \n
+ * -menuconfig on hub_project: \n
+ * menuconfig -> example connection configuration, write name and password of the wifi network, 
+ * also check that connect using wifi is set here, and not connect using ethernet \n
+ * menuconfig -> component config -> bluetooth -> check the box (otherwise the project wont compile) \n
+ * menuconfig -> component config -> ESP32-specific -> main XTAL frequency -> select autodetect \n \n
+ * 
+ * -menuconfig on main_project: \n
+ * menuconfig -> component config -> bluetooth -> check the box (otherwise the project wont compile) \n
+ * menuconfig -> component config -> ESP32-specific -> main XTAL frequency -> select autodetect \n \n
+ * 
+ * The source code for this project can be found here: 
+ * https://github.com/ben1mit/elsysG2 \n
+ * 
+ *  
+*/
+
+/**
  * @file
  * @brief this is the main file for the unit on the wing.
- * @attention 
- * some settings must be changed in makefiles and some other changes must be done. Elaborate on this later. 
  */ 
+
+
 
 #include <stdio.h>
 #include "my_mpu_wrapper.h"
